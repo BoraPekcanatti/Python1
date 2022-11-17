26ff7fdbfc8062130fec6c62142e2cb8c8a75f5f
borapekcanatti <borapekcanatti1998@gmail.com>
2022-11-17 16:45:52 +0300

    16.45

---
+++ day2homework_Pair9.py
@@ -0,0 +1,40 @@
+print("Not hesaplayacağınız ders sayısını giriniz.")
+dersSayisi=int(input())
+a=0
+b=0
+
+gecilenDersler =[]
+kalinanDersler =[]
+
+for x in range(0,dersSayisi):
+
+    print(f"{x+1}.Vize notunuzu giriniz : ")
+    vize=input()
+    print(f"{x+1}.Final notunuzu giriniz : ")
+    final=input()
+    vizeTotal1=float(vize)
+
+    finalTotal2=float(final)
+
+    vizeTotal= 0.4*vizeTotal1
+    finalTotal= 0.6*finalTotal2
+
+    totalNote= vizeTotal+finalTotal
+
+    if totalNote<50:
+        kalinanDersler.append(totalNote)
+        a=a+1
+    else :
+        gecilenDersler.append(totalNote)
+        b=b+1
+
+print(f"Kalınan ders sayısı:{a}")
+print(f"Geçilen ders sayısı:{b}")
+print(f"Kalınan dersler:{kalinanDersler}")
+print(f"Geçilen dersler:{gecilenDersler}")
+
+
+
+        
+    
+    
